50cda110a72b952241985b48bed2df5b070baa2d
NaiKeN <caioluizsouza12@gmail.com>
2022-03-31 18:22:34 -0300

    path created

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HeaderComponent } from './components/template/header/header.component';
+import { ProductCreateComponent } from './components/product/product-create/product-create.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
+import { ProductReadComponent } from './components/product/product-read/product-read.component';
+import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { FooterComponent } from './components/template/footer/footer.component';
+import { NavComponent } from './components/template/nav/nav.component';
+import { HomeComponent } from './views/home/home.component';
+import { ProductAngularappComponent } from './views/product-angularapp/product-angularapp.component';
+import { RedDirective } from './directives/red.directive';
+
+@NgModule({
+  declarations: [ //apenas entram components, diretivas e pipes -- services não!
+    AppComponent,
+    HeaderComponent,
+    ProductCreateComponent,
+    ProductDeleteComponent,
+    ProductReadComponent,
+    ProductUpdateComponent,
+    FooterComponent,
+    NavComponent,
+    HomeComponent,
+    ProductAngularappComponent,
+    RedDirective
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ frontend/src/app/views/product-angularapp/product-angularapp.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product-angularapp',
+  templateUrl: './product-angularapp.component.html',
+  styleUrls: ['./product-angularapp.component.css']
+})
+export class ProductAngularappComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
